97969abeb060207a07cc73c858c23f14bf9b2ead
Kishan patel <kishan.p@simformsolutions.com>
2022-03-29 14:06:58 +0530

    Weather APP

--- NodeJs Basics/Node JS Course/web-server/src/app.js
+++ NodeJs Basics/Node JS Course/web-server/src/app.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
 const app = express()
 
 
@@ -34,10 +38,29 @@ app.get('/help', (req, res) => {
     })
 })
 app.get('/weather', (req, res) => {
-    res.send([{
-        forecast: 'It is Raining',
-        location: 'Haridwar'
-    }])
+    if (!req.query.address) {
+
+        return res.send({
+            error: 'Please enter place'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+
+    })
 })
 
 app.get('*', (req, res) => {
